a8d4f683985bee0ba5289ba6d6ca4496f0afeb22
Oleksiy Radyvanyuk <oleksiy@ngti.nl>
2019-06-18 09:51:50 +0200

    Initial Commit

---
+++ TestLocalAlbums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestLocalAlbums
+//
+//  Created by Oleksiy Radyvanyuk on 18/06/2019.
+//  Copyright © 2019 NGTI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
